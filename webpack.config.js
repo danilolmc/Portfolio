3dbaec7eeb20bebc4e121566742eaf104616b4cf
DaniloLima122 <limadanilo06@gmail.com>
2020-08-25 16:07:54 -0300

    gerado o build e enviando os arquivos

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
 
 
             {
-                test: /\.(png|svg|jpg|gif)$/,
+                test: /\.(png|svg|jpg|gif|ico)$/,
                 use: [
 
                     "file-loader"
